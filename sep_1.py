18617611cce5b110c8461d94714344745494ee4e
TSchotter <schotter.terratoast@gmail.com>
2026-09-01 14:35:41 -0400

    new stuff

--- sep_1.py
+++ sep_1.py
@@ -1,1 +1,11 @@
-print("Hello")
+class Node:
+    def __init__(self, num): # constructor
+        self.num = num # set num to internal value
+    def get(self):
+        return self.num
+    def setNum(self, num):
+        self.num = num
+
+x = Node(5)
+x.setNum(10)
+print(x.get())
